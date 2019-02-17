4a29795739755f8bd334fe81b62563108cbe8052
Behrooz Karjoo <bkarjoo@gmail.com>
2019-02-17 09:04:51 -0500

    extracted iterative fibonacci algorithm

--- src/chapter1/section1/Exercise6.java
+++ src/chapter1/section1/Exercise6.java
@@ -6,7 +6,20 @@ import edu.princeton.cs.algs4.StdOut;
  * Created by Rene Argento
  */
 public class Exercise6 {
-
+	// note this is a fast iterative way to get fibonacci sequence 
+	// requiring only two operations per iterations
+	// this method returns the Nth fib sequence
+	public static long IterativeFib(int N)
+	{
+		long f = 0, g = 1;
+		for (long i = 0; i < N; i++)
+		{
+			f = f + g;
+			g = f - g;
+		}
+		return f;
+	}
+	
 	public static void main(String[] args) {
 		int f = 0;
 		int g = 1;
