abb0004384493aaa17a1eaa26ba1923dac638b1c
Behrooz Karjoo <bkarjoo@gmail.com>
2019-02-17 14:14:56 -0500

    offer alternate solution lg2

--- src/chapter1/section1/Exercise14.java
+++ src/chapter1/section1/Exercise14.java
@@ -25,4 +25,15 @@ public class Exercise14 {
 		return logInt - 1;
 	}
 	
+	// bottom up reaches the result 1 or 2 iterations faster
+	public static int lg2(int N)
+	{
+		int log = 1, power = 2;
+		while (power < N) {
+			power *= 2; 
+			log++;
+		}
+		return power > N ? log - 1 : log;
+	}
+	
 }
